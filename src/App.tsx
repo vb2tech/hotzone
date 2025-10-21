997e7de0ddddf96e55790e5984263af69c0f00b6
vb2tech <vb2tech@gmail.com>
2025-10-21 19:02:18 -0400

    fix mobile sizing

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const AppContent: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navigation />
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/zones" element={<ZonesPage />} />
